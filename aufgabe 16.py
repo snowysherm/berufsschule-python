3c0ed830e3bc9334e9c00c9f9f1684bdf94168e8
Tom Krause <shermkaboom@gmail.com>
2023-05-16 13:02:38 +0200

    aufgaben

--- aufgabe 16.py
+++ aufgabe 16.py
@@ -1,3 +1,14 @@
 from random import randint
 
-print(randint(1,500), randint(1,500))
+zahl1 = randint(1,100)
+zahl2 = randint(1,100)
+
+zahl3 = zahl1 + zahl2
+
+print(f"Zahl 1: {zahl1}, Zahl 2: {zahl2}")
+ergebnis_user = float(input("Bitte addieren und Ergebnis eingeben!: "))
+
+if (zahl3 == ergebnis_user):
+    print("Richtig! YIPPIE")
+else:
+    print("Nicht richtig! :(")
