7915a1e98cc30377d6fc4a614df583ae3a1a28e7
Tom Krause <shermkaboom@gmail.com>
2023-05-16 15:02:46 +0200

    aufgaben

---
+++ aufgabe 20.py
@@ -0,0 +1,4 @@
+n = int(input("Zahl: "))
+
+for stern in range(1, n + 1):
+    print("**********")
---
+++ aufgabe 21.py
@@ -0,0 +1,5 @@
+n = int(input("Zeilen: "))
+m = int(input("Sterne: "))
+
+for i in range(1, n+1):
+    print("*" * m)
