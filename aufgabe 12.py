fc51c4615b2642961c67c7e1ca682e2036b32bc8
Tom Krause <shermkaboom@gmail.com>
2023-05-16 11:18:39 +0200

    aufgabe 12

---
+++ aufgabe 12.py
@@ -0,0 +1,6 @@
+zahl1 = float(input("Zahl 1: "))
+zahl2 = float(input("Zahl 2: "))
+zahl3 = float(input("Zahl 3: "))
+zahl4 = float(input("Zahl 4: "))
+
+print("Mittelwert", zahl1 + zahl2 + zahl3 + zahl4 / 4)
