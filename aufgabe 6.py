e8867da17821ff9e95a304318afb6bf82af1e489
Tom Krause <shermkaboom@gmail.com>
2023-05-16 13:33:06 +0200

    aufgaben

--- aufgabe 6.py
+++ aufgabe 6.py
@@ -6,4 +6,4 @@ volumen = a*b*c
 oberflaeche = 2*a*b+2*a*c+2*b*c
 raumdiagonale = a**(2)+b**(2)+c**(2)**(0.5)
 
-print(f"Volumen: {volumen}, Oberflaufäche: {oberflaeche}, Raumdiagonale: {raumdiagonale}")
+print(f"Volumen: {volumen}, Oberflaufäche: {oberflaeche}, Raumdiagonale: {round(raumdiagonale, 2)}")
