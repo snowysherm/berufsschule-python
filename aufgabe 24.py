a27f7669c4feeca2752fb1fc98ea615705a1436b
Tom Krause <shermkaboom@gmail.com>
2023-05-23 14:57:10 +0200

    aufgaben

---
+++ aufgabe 24.py
@@ -0,0 +1,12 @@
+bug = float(0.0002) # kubik m
+bugs = int(input("Käfer: ")) # anzahl käfer
+haus = float(input("Haus Volumen: ")) # kubik m
+bug_vol = bugs * bug
+
+while bug_vol < haus:
+
+
+
+
+
+
